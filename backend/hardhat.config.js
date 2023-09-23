b2c78288ce2390ac44eb57fe6ccb00076d67b858
Riocodex <onwukachibike@gmail.com>
2023-09-23 21:58:26 +0100

    updated

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,9 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
-require("dotenv").config();
 
-const GOERLI_URL = process.env.GOERLI_URL;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 
 
 module.exports = {
