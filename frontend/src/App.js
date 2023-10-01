bc3d74bea1e0ff77a8cc2ddea5a4f5b3ccb5475c
Riocodex <onwukachibike@gmail.com>
2023-10-01 01:36:24 +0100

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import background from './assets/bg1.jpeg';
 
 function App() {
   return (
     <div className="App">
-      <div className="background-image">
+       <div className={"background-image"}>
         <header className="App-header">
-          HELLO! Time to build!!!!!i
+          <nav class="navbar">
+            <li>Left Item</li>
+            <li>Right Item</li>
+          </nav>
         </header> 
       </div>
     </div>
